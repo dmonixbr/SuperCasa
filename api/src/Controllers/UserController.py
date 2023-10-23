a2c957b31b85d0cd06d46a71b0714cc73e36ee99
Victor Vieira de Melo <38594782+dmonixbr@users.noreply.github.com>
2023-10-23 16:13:09 -0300

    [API] Tratamento de excecao User

--- api/src/Controllers/UserController.py
+++ api/src/Controllers/UserController.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
 import src.Services.UserService as UserService
-from datetime import datetime, timedelta
+import src.libs.HttpResponse as HttpResponse
+from datetime import timedelta
 
 user = Blueprint('user', __name__, 'user')
 
@@ -16,13 +17,13 @@ def login():
         # Se as credenciais forem válidas, crie um token JWT
         if UserService.login(username, password):
             access_token = create_access_token(identity=username, expires_delta=timedelta(minutes=60))
-            return jsonify(access_token=access_token), 200
+            return jsonify(access_token=access_token), HttpResponse.SUCCESS
         else:
-            return jsonify({"message": "Credenciais inválidas"}), 401
+            return jsonify({"message": "Credenciais inválidas"}), HttpResponse.UNAUTHORIZED
     except ValueError as e:
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"error": str(e)}), HttpResponse.BAD_REQUEST
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)}), HttpResponse.INTERNAL_SERVER_ERROR
 
 @user.route('/', methods=['POST'])
 def createUser():
@@ -31,14 +32,14 @@ def createUser():
         password:str = request.json.get('password')
 
         if not username or not password:
-            return jsonify({"message": "Dados insuficientes"}), 400
+            return jsonify({"message": "Dados insuficientes"}), HttpResponse
         
         user = UserService.createUser(username, password)
         return jsonify({"username":user.username}), 201
     except ValueError as e:
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"error": str(e)}), HttpResponse
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)}), HttpResponse.INTERNAL_SERVER_ERROR
 
 @user.route('/', methods=['PUT'])
 def updateUser():
@@ -49,14 +50,14 @@ def updateUser():
         oldPassword:str = request.json.get('oldPassword')
 
         if not username and not password and not oldPassword and not id:
-            return jsonify({"message": "Dados insuficientes"}), 400
+            return jsonify({"message": "Dados insuficientes"}), HttpResponse
         
         user = UserService.updateUser(id, username, password, oldPassword)
         if user:
-            return jsonify({"username":user.username}), 200
+            return jsonify({"username":user.username}), HttpResponse.SUCCESS
         else:
-            return jsonify({"message": "Usuário não encontrado ou senha inválida!"}), 404
+            return jsonify({"message": "Usuário não encontrado ou senha inválida!"}), HttpResponse.NOT_FOUND
     except ValueError as e:
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"error": str(e)}), HttpResponse
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)}), HttpResponse.INTERNAL_SERVER_ERROR
